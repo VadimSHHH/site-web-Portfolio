ef364b843e9136ce66d8bd539b373014781567d1
VadimSHHH <101591889+VadimSHHH@users.noreply.github.com>
2022-09-29 22:02:24 +0200

    Php contact/From

--- js/script.js
+++ js/script.js
@@ -1,33 +1,27 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__bmenu').toggleClass('active');
-        $('body').toggleClass('lock');
-  });   
-    });
-//scroll fluide
-    $(document).ready(function(){
-      // scroll fluide pour toutes ligne
-      $("a").on('click', function(event) {
-    
-        // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
-        if (this.hash !== "") {
-         
-          event.preventDefault();
-    
-          // sovgard hash 
-          var hash = this.hash;
-    
-          // methodeanimate() jQuery pour faire de scroll fluide pour la page  
-          // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-    
-            // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
-            window.location.hash = hash;
-          });
-        } // Конец, если
-      });
-    });
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__bmenu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
 
-   
+//scroll fluide
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
